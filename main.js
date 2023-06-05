9a31c0826ee42bb8de5c7f75569a503d40669b56
LeeJiSeop <jjiisub@naver.com>
2023-06-05 11:37:58 +0900

    06.05_질문 형식

--- main.js
+++ main.js
@@ -32,6 +32,15 @@ let dataQuestion = [
     content:
       "Assistant is a friendly English teacher. Assistant always replys in korean. The question format will be 'Please explain the meaning of 'word' in the given paragraph.' The answer should include the following: 1. Explanation of the meaning of the given word, 2. Grammar explanation of how the given word is used in the given paragraph, 3. Translation of the given sentence into Korean, 4. Two example sentences using the given word, along with their translations into Korean.",
   },
+  {
+    role: "user",
+    content: "As your cat, I can provide you with emotional support and companionship. Meow.에서 As의 의미를 자세히 알려줘",
+  },
+  {
+    role: "assistant",
+    content:
+      "'As'의 의미를 설명해드릴게요.\n\n1. 단어 의미\n'As' 역할이나 용도의 의미를 가집니다.\n\n2. 문법 설명\n문장에서 'As your cat'은 '당신의 고양이로서, 고양이이므로'의 뜻으로 사용되었습니다. 이 문장은 고양이가 당신에게 위로와 동반자를 제공할 수 있다는 것을 나타냅니다.\n\n3. 문장 해석\n'당신의 고양이로서, 나는 감정적 지원과 동반자를 제공할 수 있어요. 야옹.'\n\n4. 예시:\n - As a doctor, I have to follow strict hygiene guidelines. (의사로서, 저는 엄격한 위생 지침을 따라야 합니다.)\n - As a vegetarian, I don't eat meat. (채식주의자로서, 저는 고기를 먹지 않습니다.)",
+  },
 ];
 
 // 화면에 뿌려줄 데이터, 질문들
